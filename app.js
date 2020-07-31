4441d32d900c9cbffb241225c45aa2bbc08b8116
jenchihhung <jenchihhung@hotmail.com>
2020-07-31 21:12:47 +0800

    add routing

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 import navbar from './components/navbar.js';
-var app = new Vue({
+import Home from './views/home.js';
+import About from './views/about.js';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About }
+  ]
+  
+  // 3. Create the router instance and pass the `routes` option
+  // You can pass in additional options here, but let's
+  // keep it simple for now.
+  const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
+const app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!'
     },
+    router,
     components:{navbar}
   })
+
+  
