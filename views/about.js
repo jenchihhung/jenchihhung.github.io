6399a39ad7ae2081f22d6b14f5838c49098819e9
jenchihhung <jenchihhung@hotmail.com>
2020-07-31 21:27:31 +0800

    update about page

--- views/about.js
+++ views/about.js
@@ -1,1 +1,7 @@
-export default {template: '<div>about</div>'}
+export default {template: `
+<div class = "text-center">
+<h1>about</h1>
+<p>My name is Chihhung Jen, I am a software developer.</p>
+</div>
+`}
+
